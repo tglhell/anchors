c2272a2b28c81987040a148a64d4a5973a28a88d
khc <tglhell@naver.com>
2020-04-06 08:28:42 +0900

    modify

--- assets/js/index0.js
+++ assets/js/index0.js
@@ -1,24 +1,14 @@
 jQuery.event.add(window, 'load', function () {
-	var winH = $(window).height(),
-	footH = $('.footer').outerHeight(),
-	irSecM = $('body').css('font-weight') / 4,
-	irSec = irSecM * 3,
-	mainIrWrap = $('.main-ir-wrap'),
-	mainIrListW = mainIrWrap.find('.main-ir-list').width() + irSecM;
-	mainIrWrap.css('height', winH - footH);
-
 	for (i = 1; i < 6; i++) {
 		$(function (i) {
 			setTimeout(function () {
 				$('.main-ir-list').attr('ir-intro', i);
-			}, (irSec * 5) * i);
+				if (i == 5) {
+					setTimeout(function(){
+						$('.main-logo-outer').addClass('l-phase');
+					}, 2200);
+				}
+			}, 1500 * i);
 		}(i));
 	}
-
-	// $(window).resize(function(){
-	// 	var winW = $(window).outerWidth();
-	// 	if (winW < mainIrListW) {
-	// 		mainIrWrap.find('.main-ir-list').css('width', winW - irSecM);
-	// 	}
-	// });
 });
