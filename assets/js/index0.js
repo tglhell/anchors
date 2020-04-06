21f9447b8766c5e6b4a693db89b86b0dff5d4b42
khc <tglhell@naver.com>
2020-04-06 10:02:36 +0900

    modify

--- assets/js/index0.js
+++ assets/js/index0.js
@@ -1,14 +1,45 @@
 jQuery.event.add(window, 'load', function () {
+	var mIrList = $('.main-ir-list'),
+	mStepNavi = $('.main-step-navi li'),
+	irBox = setTimeout,
+	mLogoOuter = $('.main-logo-outer');
+	mainHeight ();
+	
+	$(window).resize(function(){
+		mainHeight ();
+	});
+	
 	for (i = 1; i < 6; i++) {
 		$(function (i) {
-			setTimeout(function () {
-				$('.main-ir-list').attr('ir-intro', i);
+			irBox(function () {
+				mIrList.attr('ir-intro', i);
+				mStepNavi.eq(i - 1).addClass('on').siblings().removeClass('on');
 				if (i == 5) {
-					setTimeout(function(){
-						$('.main-logo-outer').addClass('l-phase');
-					}, 2200);
+					lPhase ();
 				}
 			}, 1500 * i);
 		}(i));
+	}
+
+	mStepNavi.click(function(e){
+		var _this = $(this),
+		stepIdx = _this.index() + 1;
+		mLogoOuter.removeClass('l-phase');
+		_this.addClass('on').siblings().removeClass('on');
+		mIrList.attr('ir-intro', stepIdx);
+		if (stepIdx == 5) {
+			lPhase ();
+		}
+	});
+
+	function mainHeight () {
+		winH = $(window).height();
+		$('.wrap.main').css('height', winH);
+	}
+
+	function lPhase () {
+		irBox(function () {
+			mLogoOuter.addClass('l-phase');
+		}, 2200);
 	}
 });
