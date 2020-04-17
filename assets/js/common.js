dea525b9399d7b837f9325ddb8c5ba8bc9d8974e
unknown <tglhell@naver.com>
2020-04-17 15:46:06 +0900

    modify

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,8 @@
 jQuery.event.add(window, 'load', function () {
 	var showMainList = $('[ir] > li'),
-	winWid = $(window).width(),
-	navWid = $('.header-cont').width(),
+	winHgt = $(window).height(),
+	scrTop = $('.btn-scroll-top'),
 	aniItem = setTimeout;
-
-	$('.grid li:first').clone().insertBefore('.grid li:nth-child(1)');
-	$('.grid li:first').clone().insertBefore('.grid li:nth-child(3)');
 
 	function irInsert() {
 		$('[ir]').each(function (index) {
@@ -58,44 +55,6 @@ jQuery.event.add(window, 'load', function () {
 		}
 	});
 
-	// $('.popup').click((function () {
-	// 	var returnTar;
-	// 	return function (e) {
-	// 		var bodyWid = $('body').width();
-	// 		returnTar = $(e.target).closest('button'),
-	// 		wSize = $(window).width(),
-	// 		popIdx = $(this).attr('pop-idx');
-	// 		$('.layerPopupWrap' + '[pop-idx=' + popIdx + ']').fadeIn(500).css('display', 'table');
-	// 		$('.layerPopupCont').attr('tabindex', '0').fadeIn(500);
-	// 		if (wSize > 1024) {
-	// 			$('body').css({ 'overflow': 'hidden', 'width': bodyWid });
-	// 		}
-	// 		aniItem(function () {
-	// 			$('.layerPopupCont').focus().append('<a href="#" class="tarLoop"></a>');
-	// 			$('.tarLoop').focusin(function () {
-	// 				$('.layerPopupCont').focus();
-	// 			});
-	// 		}, 0);
-
-	// 		$('.popupClose, .layerPopupWrap').click(function (e) {
-	// 			var tarItem = $('.layerPopupCont > div, .layerTitle, .layerCont *');
-	// 			if (!$(e.target).is(tarItem)) {
-	// 				$('.layerPopupWrap').fadeOut(500);
-	// 				$('.layerPopupCont').removeAttr('tabindex').fadeOut(500);
-	// 				$('.tarLoop').remove();
-	// 				aniItem(function () {
-	// 					returnTar.focus();
-	// 				}, 0);
-	// 				aniItem(function () {
-	// 					if (wSize > 1024) {
-	// 						$('body').css({ 'overflow': 'auto', 'width': 'auto' });
-	// 					}
-	// 				}, 800);
-	// 			}
-	// 		});
-	// 	}
-	// })());
-
 	$(window).scroll(function () {
 		var scrPos = $(this).scrollTop(),
 		scrPosSum = $(document).height() - $(window).height() - $('.footer').height();
@@ -112,6 +71,16 @@ jQuery.event.add(window, 'load', function () {
 				$('.wrap').removeClass('side-fixed');
 			}
 		}
+
+		if (scrPos > winHgt) {
+			scrTop.addClass('active');
+		} else {
+			scrTop.removeClass('active');
+		}
+	});
+
+	scrTop.click(function(){
+		$('html, body').animate({ scrollTop: '0' }, '0');
 	});
 
 	// if (winWid > 767) {
