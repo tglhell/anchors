7e0638a9d04f06530840e55131443c386a9fa725
khc <tglhell@naver.com>
2021-01-27 17:10:32 +0900

    test

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,3 +1,12 @@
 jQuery.event.add(window, 'load', function () {
-	
+	setInterval(function(){
+		$('.main-scr-outer').append('<p class="main-scr-text"><marquee loop="1" scrollamount="5" direction="up"><strong>Anything is Possible</strong></marquee></p>');
+	}, 25000);
+
+	setTimeout(function(){
+		$('.main-scr-text:nth-child(1)').remove();
+			setInterval(function(){
+				$('.main-scr-text:nth-child(1)').remove();
+			}, 25000);
+	}, 50000);
 });
